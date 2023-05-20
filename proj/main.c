bf65a8a01a3dfbfc90818aab20daa34b1a5f7932
SkullSkif <likvlad662@gmail.com>
2023-05-20 19:33:50 +0700

    proj0.1

---
+++ proj/main.c
@@ -0,0 +1,277 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#define MAXSTR 255
+
+struct heap {
+    char name;
+    int stock;
+};
+/* Структура которая хранит название кучи и количество предметов в ней. Будет
+ хранить актуальные данные о кучах*/
+typedef struct heap heap;
+
+struct player {
+    char nameOfPile;
+    int countOfItems;
+};
+/* Структура которая хранит название кучи и количество предметов в ней. Данные
+ из этой структуры получены от игрока и используются только для того, чтобы
+ отнять нужное количество(countOfItems) из кучи(nameOfPile)*/
+typedef struct player player;
+
+heap* getPiles()
+{
+    int pile_count = 3; // Количество куч
+    heap* pile_status = (heap*)calloc(
+            pile_count,
+            sizeof(heap)); // Динамически выделяю память под массив структур куч
+    pile_status[0].name = 'A';
+    pile_status[1].name = 'B';
+    pile_status[2].name = 'C';
+    char count[255] = "";
+    int num = 0;
+    printf("Сколько должно быть предметов в куче?: ");
+    int err = 0;
+    while (err == 0) {
+        system("clear");
+        scanf("%s", count);
+        for (int i = 0; i < (int)strlen(count); i++) {
+            while ((count[i] < '0') || (count[i] > '9')) {
+                system("clear");
+                printf("Число задано не корректно, повторите попытку: ");
+                scanf("%s", count);
+            }
+        }
+        num = atoi(count);
+        if (num == 0) {
+            printf("Число задано не корректно, повторите попытку: ");
+            err = 0;
+        }
+        if (num < 0) {
+            printf("Число должно быть больше нуля и при этом не превышать "
+                   "максимальное значение(1874919423), повторите попытку: ");
+            err = 0;
+        }
+        if (num > 0)
+            err = 1;
+    }
+    num = atoi(count);
+
+    for (int i = 0; i < pile_count; i++)
+        pile_status[i].stock = num;
+    return pile_status;
+}
+
+void interface(heap* pile_status)
+{
+    system("clear");
+    printf("[Для выхода нажмите q]\n");
+    printf("|");
+    for (int i = 0; i < 3; i++) {
+        if (pile_status[i].stock != 0)
+            printf(" %c%d |", pile_status[i].name, pile_status[i].stock);
+    }
+    printf("\n");
+}
+
+char* errorCheck(char* strPile, heap* pile_status)
+{
+    int q = 0;
+    __fpurge(stdin);
+    for (q = 0; (strPile[q] = getchar()) != '\n'; q++)
+        ;
+    strPile[q] = '\n';
+    int i = 1;
+    int num = 0;
+    while (i != (int)strlen(strPile)) {
+        if (((toupper(strPile[0]) != 'A') || (pile_status[0].stock <= 0))
+            && ((toupper(strPile[0]) != 'B') || (pile_status[1].stock <= 0))
+            && ((toupper(strPile[0]) != 'C') || pile_status[2].stock <= 0)) {
+            system("clear");
+            if (strPile[0] == 'q' && strPile[1] == '\n')
+                exit(0);
+            interface(pile_status);
+            if ((toupper(strPile[0]) > 67) && (toupper(strPile[0]) < 91))
+                printf("\nТакой кучи нет, повторите попытку: ");
+            else
+                printf("\nКуча задана не корректно формату [имя][кол-во], "
+                       "повторите попытку: ");
+            __fpurge(stdin);
+            for (q = 0; (strPile[q] = getchar()) != '\n'; q++)
+                ;
+            strPile[q] = '\n';
+                        if (strPile[0] == 'q' && strPile[1] == '\n')
+                exit(0);
+            i = 1;
+        }
+        while (strPile[i] == ' ')
+            i++;
+        while (strPile[i] == '0' && num == 0) {
+            i++;
+            if (strPile[i] == '\n') {
+                system("clear");
+                interface(pile_status);
+                printf("\nВы не можете забрать меньше одной спички, повторите "
+                       "попытку: ");
+                __fpurge(stdin);
+                for (q = 0; (strPile[q] = getchar()) != '\n'; q++)
+                    ;
+                strPile[q] = '\n';
+                            if (strPile[0] == 'q' && strPile[1] == '\n')
+                exit(0);
+                i = 1;
+            }
+        }
+        if ((strPile[i] == '-') && (num == 0)) {
+            system("clear");
+            interface(pile_status);
+            printf("\nВы не можете забрать меньше одной спички, повторите "
+                   "попытку: ");
+            __fpurge(stdin);
+            for (q = 0; (strPile[q] = getchar()) != '\n'; q++)
+                ;
+            strPile[q] = '\n';
+                        if (strPile[0] == 'q' && strPile[1] == '\n')
+                exit(0);
+            i = 1;
+        }
+        if ((strPile[i] == '-') && (num == 1)) {
+            system("clear");
+            interface(pile_status);
+            printf("\nКуча задана не корректно формату [имя][кол-во], "
+                   "повторите попытку: ");
+            __fpurge(stdin);
+            for (q = 0; (strPile[q] = getchar()) != '\n'; q++)
+                ;
+            strPile[q] = '\n';
+                        if (strPile[0] == 'q' && strPile[1] == '\n')
+                exit(0);
+            i = 1;
+        }
+
+        if (((strPile[i] >= '1') && (strPile[i] <= '9'))
+            || ((strPile[i] == '0') && (num == 1))) {
+            num = 1;
+            i++;
+        }
+        if (strPile[i] == '\n')
+            break;
+        if (((strPile[i] < '0') || (strPile[i] > '9')) && (num == 1)) {
+            system("clear");
+            interface(pile_status);
+            printf("\nКуча задана не корректно формату [имя][кол-во], "
+                   "повторите попытку: ");
+            __fpurge(stdin);
+            for (q = 0; (strPile[q] = getchar()) != '\n'; q++)
+                ;
+            strPile[q] = '\n';
+                        if (strPile[0] == 'q' && strPile[1] == '\n')
+                exit(0);
+            i = 1;
+        }
+        if ((strPile[i] == '0') && (strPile[i + 1] == '\n') && (num == 0)
+            && (strPile[i] != ' ')) {
+            system("clear");
+            interface(pile_status);
+            printf("\nВы не можете забрать меньше одной спички, повторите "
+                   "попытку: ");
+            __fpurge(stdin);
+            for (q = 0; (strPile[q] = getchar()) != '\n'; q++)
+                ;
+            strPile[q] = '\n';
+                        if (strPile[0] == 'q' && strPile[1] == '\n')
+                exit(0);
+            i = 1;
+        }
+        if (num == 0) {
+            system("clear");
+            interface(pile_status);
+            printf("\nКуча задана не корректно формату [имя][кол-во], "
+                   "повторите попытку: ");
+            __fpurge(stdin);
+            for (q = 0; (strPile[q] = getchar()) != '\n'; q++)
+                ;
+            strPile[q] = '\n';
+                        if (strPile[0] == 'q' && strPile[1] == '\n')
+                exit(0);
+            i = 1;
+        }
+    }
+
+    system("clear");
+    return strPile;
+}
+
+player* getData(char* strPile)
+{
+    player* move = (player*)calloc(
+            1,
+            sizeof(player)); /* Выделяю память для структуры, в которую запишу
+                              данные в нужной форме, считанные с клавиатуры*/
+    char number[MAXSTR] = {}; /* Объявляю строку, куда буду помещать число
+                               написанное строкой*/
+    int j = 0; // Счетчик для прохождения по number
+    strPile[0] = toupper(strPile[0]); /* Корректируем ввод на случай, если куча
+                                       была написана с маленькой буквы*/
+    move->nameOfPile = strPile[0]; // Вытаскиваем из строки имя кучи
+    for (int i = 1;
+         i <= (int)strlen(strPile); // Записываю число из строки в number
+         i++) {
+        number[j] = strPile[i];
+        j++;
+    }
+    j = 0;
+    move->countOfItems = atoi(number); /* Перевожу число строкой в тип int и
+                                        записываю ее в кучу move*/
+
+    return move; /* Здесь move это та же куча, что и в pile_status, но созданная
+                  конкретно как единичная куча, которую ввел игрок.*/
+}
+
+void game(heap* pile_status)
+{
+    char strPile[MAXSTR]; /* Объявляю строку, в которой будет хранится
+                                считанная с клавы строка*/
+    char* str;
+    char playername
+            = '2'; /* playername - имя текущего игрока. Начинаю со второго, т.к
+                    в начале цикла буду делать переход на следующего игрока.*/
+    while ((pile_status[0].stock + pile_status[1].stock
+            + pile_status[2].stock) /* В этом огромном цикле, который, спасибо
+                                     кленг-формату, превратился в белеберду,
+                                     идет игра, пока все кучи не закончатся*/
+           > 0) {
+        if (playername == '1') // Здесь меняется игрок
+            playername = '2';
+        else
+            playername = '1';
+
+        interface(pile_status);
+        printf("Ход игрока %c\n", playername);
+        str = errorCheck(strPile, pile_status);
+        player* move = getData(str); // Здесь игрок пишет с клавиатуры кучу
+        for (int i = 0; i <= 3; i++) {
+            if (move->nameOfPile == pile_status[i].name)
+                pile_status[i].stock -= move->countOfItems;
+        } // В цикле выше если совпало имя, то отнимаем нужное кол-во предметов
+        for (int i = 0; i <= 3; i++) // Обнуляем кучу, если ушли в минус
+            if (pile_status[i].stock < 0)
+                pile_status[i].stock = 0;
+        free(move);
+    }
+    system("clear");
+    printf("\nПобеда игрока %c\n", playername); // Кучи кончились, игрок победил
+}
+
+int main()
+{
+    heap* pile_status = getPiles();
+    game(pile_status);
+    free(pile_status);
+    return 0;
+}
--- proj/test.c
+++ proj/test.c
@@ -1,81 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ncurses.h>
+#define MAXSTR 255
 #define x 90
 #define y 35
 #define middle ((int)x / 3)
 #define indent 6
 #define width 5
 
+// void mainmenu()
+// {
+//     char interface[y][x];
+//     int i;
+//     unsigned char key = ' ';
+//     while (key!='q')
+//     {
+//     system("clear");
+//     // Окно игры
+//     for (i = 0; i < y; i++)
+//         for (int j = 0; j < x; j++)
+//             interface[i][j] = ' ';
+//     for (i = 0; i < y; i++) {
+//         interface[i][0] = '#';
+//         interface[i][x - 1] = '#';
+//     }
+//     for (i = 0; i < x; i++) {
+//         interface[0][i] = '#';
+//         interface[y - 1][i] = '#';
+//     }
+
+//     // Буквы с названием игры
+//     for (i = 3; i < 8; i++) {
+//         interface[i][middle] = '#';
+//         interface[i][middle + width] = '#';
+//         interface[i][middle + width + indent] = '#';
+//         interface[i][middle + width + indent + width] = '#';
+//         interface[i][middle + width + indent + width + indent] = '#';
+//         interface[i][middle + width + indent + width + indent + width + 1]
+//                 = '#';
+//     }
+
+//     for (i = middle + 1; i < middle + width; i++)
+//         interface[5][i] = '#';
+
+//     interface[6][middle + width + indent + 1] = '#';
+//     interface[5][middle + width + indent + 2] = '#';
+//     interface[4][middle + width + indent + 3] = '#';
+
+//     interface[3][middle + width + indent + width + indent + 1] = '#';
+//     interface[4][middle + width + indent + width + indent + 2] = '#';
+//     interface[5][middle + width + indent + width + indent + 3] = '#';
+//     interface[4][middle + width + indent + width + indent + 4] = '#';
+//     interface[3][middle + width + indent + width + indent + 5] = '#';
+//         for (int i = 0; i < y; i++) {
+//             for (int j = 0; j < x; j++)
+//                 printf("%c", interface[i][j]);
+//             printf("\n");
+//         }
+//     key=getchar();
+//     }
+// }
+
 void mainmenu()
 {
-    char interface[y][x];
+    // initscr();
+    // WINDOW* screen = newwin(5, 5, 10, 10);
+    // box(screen, 0, 0);
+    // wrefresh(screen);
+    // noecho();
+    // curs_set(0);
+    // printw("@");
+    // getch();
+    // endwin();
     int i;
-    unsigned char key = ' ';
-    while (key!='q')
-    {
-    system("clear");
-    // Окно игры
-    for (i = 0; i < y; i++)
-        for (int j = 0; j < x; j++)
-            interface[i][j] = ' ';
-    for (i = 0; i < y; i++) {
-        interface[i][0] = '#';
-        interface[i][x - 1] = '#';
-    }
-    for (i = 0; i < x; i++) {
-        interface[0][i] = '#';
-        interface[y - 1][i] = '#';
-    }
-
-    // Буквы с названием игры
-    for (i = 3; i < 8; i++) {
-        interface[i][middle] = '#';
-        interface[i][middle + width] = '#';
-        interface[i][middle + width + indent] = '#';
-        interface[i][middle + width + indent + width] = '#';
-        interface[i][middle + width + indent + width + indent] = '#';
-        interface[i][middle + width + indent + width + indent + width + 1]
-                = '#';
-    }
-
-    for (i = middle + 1; i < middle + width; i++)
-        interface[5][i] = '#';
-
-    interface[6][middle + width + indent + 1] = '#';
-    interface[5][middle + width + indent + 2] = '#';
-    interface[4][middle + width + indent + 3] = '#';
-
-    interface[3][middle + width + indent + width + indent + 1] = '#';
-    interface[4][middle + width + indent + width + indent + 2] = '#';
-    interface[5][middle + width + indent + width + indent + 3] = '#';
-    interface[4][middle + width + indent + width + indent + 4] = '#';
-    interface[3][middle + width + indent + width + indent + 5] = '#';
-    //  for (i = 3;i<8;i++)
-    //      {
-    //          interface[i][27] = '#';
-    //          interface[i][32] = '#';
-    //      }
-    // interface[4][21] = '#';
-    // interface[5][22] = '#';
-    // interface[6][23] = '#';
-
-    // interface[3][26] = '#';
-    // for (int i = 5;i<8;i++)
-    //     interface[i][26] = '#';
-    // key=getch();
-        
-        for (int i = 0; i < y; i++) {
-            for (int j = 0; j < x; j++)
-                printf("%c", interface[i][j]);
-            printf("\n");
-        }
-    key=getchar();
-    }
+    char str[20];
+ 
+    for (i=0; (str[i]=getchar())!='\n'; i++);
+    str[i] = '\0';
+    int h = atoi(str);
+    printf("\n%d\n", h);
 }
+
 
 int main()
 {
     mainmenu();
-    system("clear");
+   // system("clear");
     return 0;
 }
